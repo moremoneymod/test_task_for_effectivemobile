18fb322e65f5c09469f4b08c07f2301aadf1bdd0
moremoneymod <84ivan48@gmail.com>
2024-11-24 02:39:27 +0300

    добавлены методы для класса BookManager, а также переписана документация

--- src/classes/book.py
+++ src/classes/book.py
@@ -1,14 +1,13 @@
-import time
-
-
 class Book:
+    """Класс, отвечающий за представление книги"""
     book_id: int
     title: str
     author: str
     year: int
     status: str
 
-    def __init__(self, book_id: int, title: str, author: str, year: int, status: str):
+    def __init__(self, book_id: int, title: str, author: str, year: int, status: str) -> None:
+        """Метод-конструктор, принимает на вход данные книги"""
         self.book_id = book_id
         self.title = title
         self.author = author
